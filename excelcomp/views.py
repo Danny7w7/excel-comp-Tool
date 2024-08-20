13a33192606a836407b1258408697a112d2917f6
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2024-08-20 15:26:33 -0500

    add funtion report broker

--- excelcomp/views.py
+++ excelcomp/views.py
@@ -11,8 +11,12 @@ import os
 
 def index(request):
     if request.method == 'POST':
-        files = [request.FILES['file1'], request.FILES['file2']]
         dfs = []
+        if request.POST['report'] == 'client_report':
+            files = [request.FILES['file1'], request.FILES['file2']]
+        elif request.POST['report'] == 'broker_report':
+            files = [request.FILES['file1']]
+
         for file in files:
             extension = get_extension(file)
             if extension == '.xlsx':
@@ -24,34 +28,112 @@ def index(request):
 
 def report_client_report(df):
     df.columns = df.columns.str.strip()
-    filtered_df = df[df['Provider Status'] != 'Active']
-    remaining_df = df[df['Provider Status'] == 'Active']
+    filtered_df = df[df['Status'] != 'Active']
+    remaining_df = df[df['Status'] == 'Active']
     return remaining_df, filtered_df
+
+def report_broker_report(df):
+    date_columns = ['Received Date', 'Issue Date', 'Effective Date', 'Paid to Date', 'Subscriber DOB']
+
+    # Estandarizar las fechas en las columnas especificadas al formato MM/DD/YYYY
+    for col in date_columns:
+        if col in df.columns:
+            df[col] = pd.to_datetime(df[col], errors='coerce').dt.strftime('%m/%d/%Y')
+
+
+    df.columns = df.columns.str.strip()
+    active = df[df['Status'] == 'Active']
+    active_grace_period = df[df['Status'] == 'Active (Grace Period) (Payment Error)']
+    void_return = df[df['Status'] == 'Void-Return-Existing Insurance']
+    active_but_date = df[(df['Status'] == 'Active (Payment Error)') & (df['Paid to Date'].notna()) & (df['Paid to Date'] != '')]
+    active_but_no_date = df[(df['Status'] == 'Active (Payment Error)') & (df['Paid to Date'].isna() | (df['Paid to Date'] == ''))]
+    automatic_termination = df[df['Status'] == 'Automatic Termination (Payment Error)']
+    other_case = df[(df['Status'].notna()) &
+                    (df['Status'] != '') &
+                    (df['Status'] != 'Active') & 
+                    (df['Status'] != 'Active (Payment Error)') & 
+                    (df['Status'] != 'Active (Grace Period) (Payment Error)') & 
+                    (df['Status'] != 'Void-Return-Existing Insurance') &
+                    (df['Status'] != 'Automatic Termination (Payment Error)')]
+
+
+    return active, active_grace_period, void_return, active_but_date, active_but_no_date, automatic_termination, other_case
 
 def generate_excel(dfs, request, nameUwU):
     remaining_dfs = []
     filtered_dfs = []
+    actives = []
+    actives_grace_period = []
+    voids_return = []
+    actives_but_date = []
+    actives_but_no_date = []
+    automatic_terminations = []
+    others_case = []
 
     if request.POST['report'] == 'client_report':
         for df in dfs:
             remaining_df, filtered_df = report_client_report(df)
             remaining_dfs.append(remaining_df)
             filtered_dfs.append(filtered_df)
+    elif request.POST['report'] == 'broker_report':
+        for df in dfs:
+            active, active_grace_period, void_return, active_but_date, active_but_no_date, automatic_termination, other_case = report_broker_report(df)
+            actives.append(active)
+            actives_grace_period.append(active_grace_period)
+            voids_return.append(void_return)
+            actives_but_date.append(active_but_date)
+            actives_but_no_date.append(active_but_no_date)
+            automatic_terminations.append(automatic_termination)
+            others_case.append(other_case)
 
     # Crear un nuevo archivo Excel
     output = BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
         # Escribir los DataFrames originales y filtrados en nuevas hojas
-        sheet_names = {
-            0: ("ACTIVE LAP & TRU", "CANCELADOS LAP & TRU"),
-            1: ("ACTIVE SECURE", "CANCELADOS SECURE")
-        }
-        for i, (remaining_df, filtered_df) in enumerate(zip(remaining_dfs, filtered_dfs)):
-            remaining_df.to_excel(writer, sheet_name=sheet_names[i][0], index=False)
-            filtered_df.to_excel(writer, sheet_name=sheet_names[i][1], index=False)
+        
+        if request.POST['report'] == 'client_report':
+            sheet_names = {
+                0: ("ACTIVE LAP & TRU", "CANCELADOS LAP & TRU"),
+                1: ("ACTIVE SECURE", "CANCELADOS SECURE")
+            }
+            for i, (remaining_df, filtered_df) in enumerate(zip(remaining_dfs, filtered_dfs)):
+                if not remaining_df.empty:
+                    remaining_df.to_excel(writer, sheet_name=sheet_names[i][0], index=False)
+                if not filtered_df.empty:
+                    filtered_df.to_excel(writer, sheet_name=sheet_names[i][1], index=False)
+                    
+        elif request.POST['report'] == 'broker_report':
+            sheet_names = {
+                0: "Active Clients",
+                1: "Actives Clients with payment",
+                2: "Actives Clients Without payment",
+                3: "Actives Clients in grace period",
+                4: "Automatic Termination",
+                5: "Void return",
+                6: "Other cases",
+            }
+            for i, (active_client, active_grace_period, void_return, active_but_date, active_but_no_date, automatic_termination, other_case) in enumerate(zip(actives, actives_grace_period, voids_return, actives_but_date, actives_but_no_date, automatic_terminations, others_case)):
+                print(actives_but_date)
+                if not active_client.empty:
+                    active_client.to_excel(writer, sheet_name=sheet_names[0], index=False)
+                if not active_grace_period.empty:
+                    active_grace_period.to_excel(writer, sheet_name=sheet_names[3], index=False)
+                if not void_return.empty:
+                    void_return.to_excel(writer, sheet_name=sheet_names[5], index=False)
+                if not active_but_date.empty:
+                    active_but_date.to_excel(writer, sheet_name=sheet_names[1], index=False)
+                if not active_but_no_date.empty:
+                    active_but_no_date.to_excel(writer, sheet_name=sheet_names[2], index=False)
+                if not automatic_termination.empty:
+                    automatic_termination.to_excel(writer, sheet_name=sheet_names[4], index=False)
+                if not other_case.empty:
+                    other_case.to_excel(writer, sheet_name=sheet_names[6], index=False)
+        
+        
+        # Verificar si hay hojas en el archivo
+        if len(writer.sheets) == 0:
+            raise ValueError("No se pudo crear el archivo Excel, ya que no hay hojas visibles.")
 
-
-    # Configurar el puntero del buffer al inicio
     output.seek(0)
 
     # Crear la respuesta HTTP con el archivo Excel
---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todos los elementos <select> en la página
+    var selectReport = document.getElementById('selectReport');
+
+    selectReport.addEventListener('change', function() {
+        if (selectReport.value == 'client_report'){
+            console.log("Entro aqui marico")
+            createFileInputs('Lapeira & Tru', 'SecurePlus')
+        }else if (selectReport.value == 'broker_report'){
+            console.log("Este entro aki huevon")
+            createFileInputs("Broker Report")
+        }else{
+            console.log("etees el elses mrk")
+            createFileInputs()
+        }
+    });
+});
+
+function createFileInputs(text1, text2) {
+    const container = document.getElementById("inputsFiles");
+    container.innerHTML = ''
+    if (text1 === undefined) {
+        container.innerHTML = ''
+        return;
+    }
+    
+    const outerDiv = document.createElement("div");
+    outerDiv.className = "mb-3";
+
+    const outerDiv2 = document.createElement("div");
+    outerDiv2.className = "mb-3";
+
+    const label1 = document.createElement("label");
+    label1.className = "form-label";
+    label1.textContent = text1;
+    const input1 = document.createElement("input");
+    input1.name = "file1";
+    input1.className = "form-control";
+    input1.type = "file";
+    if (text1 == 'Broker Report') {
+        input1.accept = ".xlsx";
+    }else{
+        input1.accept = ".csv";
+    }
+    
+    
+    
+    const label2 = document.createElement("label");
+    label2.className = "form-label";
+    label2.textContent = "SecurePlus";
+    
+    const input2 = document.createElement("input");
+    input2.name = "file2";
+    input2.className = "form-control";
+    input2.type = "file";
+    input2.accept = ".csv";
+    
+    outerDiv.appendChild(label1);
+    outerDiv.appendChild(input1);
+    outerDiv2.appendChild(label2);
+    outerDiv2.appendChild(input2);
+    
+    container.appendChild(outerDiv);
+    
+    if (text2 !== undefined) {
+        container.appendChild(outerDiv2);
+    }
+}
